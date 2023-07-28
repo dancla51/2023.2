62877c3ad22dd644bcc65a4a207addbd25b3438c
dancla51 <dcla189@aucklanduni.ac.nz>
2023-07-28 16:00:23 +1200

    Plot Results but can't see it

--- 331Lab1/Lab1_Q3.py
+++ 331Lab1/Lab1_Q3.py
@@ -1,6 +1,6 @@
 import numpy as np
 import Lab1
-
+import matplotlib.pyplot as plt
 
 def construct_system(N, K):
     # K should be a list
@@ -17,18 +17,38 @@ def construct_system(N, K):
                     A[i,j] = -1 * K[j]
     return(A)
 
+
+def plot_results(real, tols, founds):
+
+    # Plot found values
+    for i, tol in enumerate(tols):
+        plt.plot([tol]*10, founds[1], 'b^')
+    # Plot real values (with horizontal lines)
+    for ele in real:
+        plt.plot([1e-8, 1e-2], [ele]*2, 'r-')
+
+    plt.xlabel("Tolerance")
+    plt.ylabel("Frequency (Hz)")
+    plt.semilogx()
+    plt.show()
+
+
 def solve_freq_modes(N,K):
     A_mat = construct_system(N, K)
-    all_eigenpairs = Lab1.power_w_deflate(A_mat, 10**-2)
-    found_eig = []
-    for pair in range(N):
-        found_eig.append(all_eigenpairs[pair][0])
-    found_freq = np.sqrt(found_eig) / (2*np.pi)
+    tols = [1e-2, 1e-4, 1e-6, 1e-8]
+    freqs_from_tols = []
+
+    for num, tol in enumerate(tols):
+        all_eigenpairs = Lab1.power_w_deflate(A_mat, tol)
+        found_eig = []
+        for pair in range(N):
+            found_eig.append(all_eigenpairs[pair][0])
+        found_freq = np.sqrt(found_eig) / (2*np.pi)
+        freqs_from_tols.append(found_freq)
 
     real_eig = np.sort(np.linalg.eig(A_mat)[0])[::-1]
     real_freq = np.sqrt(real_eig) / (2*np.pi)
-    print(real_freq)
-    print(found_freq)
+    plot_results(real_freq, tols, freqs_from_tols)
 
 
 if __name__ == "__main__":
