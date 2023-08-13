44588c9eaa34e4ca078a244ff17d0b07c2b27149
dancla51 <dcla189@aucklanduni.ac.nz>
2023-08-13 13:47:29 +1200

    Updates from Andrew

--- 331Lab2/task1.py
+++ 331Lab2/task1.py
@@ -2,6 +2,7 @@
 
 from golden import golden
 from brent import brent
+from fullbrent import fullbrent
 from functions import task1functions, task1functiontitles, task3functions, task3functiontitles
 from testoptimiser import testoptimiser
 
@@ -13,9 +14,10 @@ tol = 10e-4
 max_iter = 50
 a = 0.0   # a & b define uncertainty interval
 b = 1.0
-plotleft = 0.0 # plotleft & plotright define the plot range
+plotleft = 0.0  # plotleft & plotright define the plot range
 plotright = 3.0
 showlog = True
 
-testoptimiser(golden,"Golden Section", task1functions, task1functiontitles, a, b, max_iter, tol, showlog, plotleft, plotright)
-testoptimiser(brent,"Brents Method",  task1functions, task1functiontitles, a, b, max_iter, tol, showlog, plotleft, plotright)
+testoptimiser(golden,"Andrew's Golden", task1functions, task1functiontitles, a, b, max_iter, tol, showlog, plotleft, plotright)
+testoptimiser(brent,"Andrew's Brent", task1functions, task1functiontitles, a, b, max_iter, tol, showlog, plotleft, plotright)
+testoptimiser(fullbrent,"My FullBrent",  task1functions, task1functiontitles, a, b, max_iter, tol, showlog, plotleft, plotright)
