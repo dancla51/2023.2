ba12ede334de10123e0e2f82899368f78abe8042
dancla51 <dcla189@aucklanduni.ac.nz>
2023-09-28 11:04:35 +1300

    Finished bisection

--- 331Lab4_NLE/NLELab_Part1/algorithms.py
+++ 331Lab4_NLE/NLELab_Part1/algorithms.py
@@ -1,5 +1,6 @@
 from core import *
 
+
 # Nonlinear equation root finding by the bisection method.
 # Inputs
 # f        : nonlinear function
@@ -21,51 +22,72 @@ from core import *
 # Update the bracket each iteration
 
 def bisection(f, xl, xr, max_iter, tol):
-    x=xl
-    fxl=f(xl)
-    
+    x = xl
+    fxl = f(xl)
+
     # check the left side of interval for convergence
-    if abs(fxl)<=tol:
-        return x,0,ExitFlag.Converged
+    if abs(fxl) <= tol:
+        return x, 0, ExitFlag.Converged
 
-    x = [x,xr]
-    
+    x = [x, xr]
+
     fxr = f(xr)
-    
-    # check the right side of interval for convergence / check that the l/r function values have different signs.
-    if abs(fxr)<=tol:
-        return x,0,ExitFlag.Converged
-    elif fxl*fxr>0:
-        return x,0,ExitFlag.NoRoot
 
-    i=1
+    # check the right side of interval for convergence / check that the l/r function values have different signs.
+    if abs(fxr) <= tol:
+        return x, 0, ExitFlag.Converged
+    elif fxl * fxr > 0:
+        return x, 0, ExitFlag.NoRoot
+
+    i = 1
+    x_carry = x[0]
     while True:
-        i+=1
-        
+        i += 1
+
         # compute new estimate appending to x, and evaluate the function value fx
         # Your code goes here #
-    
-        if abs(fx)<=tol:
-            return x,i-1,ExitFlag.Converged
-        elif i-1 == max_iter:
-            return x,i-1,ExitFlag.MaxIterations
+        newx = (x[-1]+x_carry) / 2
+
+        fx = f(newx)
+        if fx*f(x_carry) < 0:
+            x_carry = x_carry
+        else:
+            x_carry = x[-1]
+
+        x.append(newx)
+
+        if abs(fx) <= tol:
+            return x, i - 1, ExitFlag.Converged
+        elif i - 1 == max_iter:
+            return x, i - 1, ExitFlag.MaxIterations
 
         # update interval
         # Your code goes here #
 
-# Nonlinear equation root finding by the secant method.
-# Inputs
-# f        : nonlinear function
-# x0, x1   : initial root bracket
-# max_iter : maximum number of iterations performed
-# tol      : numerical tolerance used to check for root
-# Outputs
-# x        : one-dimensional array containing estimates of root
-# i        : number of iterations (number of times a new point is attempted to be estimated)
-# e        : ExitFlag (enumeration)
 
 def secant(f, x0, x1, max_iter, tol):
-    return
+    # Nonlinear equation root finding by the secant method.
+    # Inputs
+    # f        : nonlinear function
+    # x0, x1   : initial root bracket
+    # max_iter : maximum number of iterations performed
+    # tol      : numerical tolerance used to check for root
+    # Outputs
+    # x        : one-dimensional array containing estimates of root
+    # k        : number of iterations (number of times a new point is attempted to be estimated)
+    # e        : ExitFlag (enumeration)
+
+    x = [x0, x1]
+    k = 0
+
+
+
+
+
+
+
+
+
 
 # Nonlinear equation root finding by the Regula falsi method.
 # Inputs
@@ -81,6 +103,7 @@ def secant(f, x0, x1, max_iter, tol):
 def regula_falsi(f, xl, xr, max_iter, tol):
     return
 
+
 # Nonlinear equation root finding by Newton's method
 # Inputs
 # f        : nonlinear function
@@ -95,6 +118,7 @@ def regula_falsi(f, xl, xr, max_iter, tol):
 
 def newton(f, g, x0, max_iter, tol):
     return
+
 
 # Nonlinear equation root finding by the combined bisection/Newton's method
 # Inputs
