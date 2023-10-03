6e655c0247836788c7c652719fdcc0b1da1269d0
Daniel Clark <dcla189@UoA.auckland.ac.nz>
2023-10-03 15:05:51 +1300

    finished? Jaamsim and Java model

--- 355JaamSim/sim/sim_custom/labs/dcla189_ControlUnit.java
+++ 355JaamSim/sim/sim_custom/labs/dcla189_ControlUnit.java
@@ -3,6 +3,9 @@ package labs;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import com.jaamsim.units.DimensionlessUnit;
 
 import hccm.controlunits.ControlUnit;
 import hccm.controlunits.ControlUnit.ActivityStartCompare;
@@ -10,43 +13,89 @@ import hccm.entities.ActiveEntity;
 
 public class dcla189_ControlUnit extends ControlUnit {
 	
-	public void StartWaitForTurnstile(List<ActiveEntity> ents, double simTime) {
+	public void OnStartWaitForTurnstile(List<ActiveEntity> ents, double simTime) {
 		
+		// Get all idle turnstiles
 		ArrayList<ActiveEntity> idleTurns = this.getEntitiesInActivity("Turnstile", "WaitForPerson", simTime);
 		ActivityStartCompare actStartComp = this.new ActivityStartCompare();
 		
+		ActiveEntity person = ents.get(0);
+		double turnstileID = getNumAttribute(person, "Turnstile", simTime, -1);
+		
 		if (idleTurns.size() > 0) {
-			Collections.sort(idleTurns, actStartComp);
+			// See if correct ID turnstile is free
+			List<ActiveEntity> thisTurnstile = idleTurns
+					  .stream()
+					  .filter(q -> getNumAttribute(q, "ID", simTime, -1) == turnstileID)
+					  .collect(Collectors.toList());
 			
-			ActiveEntity person = ents.get(0);
-			ActiveEntity turnstile = idleTurns.get(0);
+			ActiveEntity turnstile = thisTurnstile.get(0);
 					
 			transitionTo("Entry", person, turnstile);
 		}
 	}
 	
-	public void StartWaitForPerson(List<ActiveEntity> ents, double simTime) {
+	public void OnStartWaitForPerson(List<ActiveEntity> ents, double simTime) {
 		
 		ArrayList<ActiveEntity> idlePeople = this.getEntitiesInActivity("Person", "WaitForTurnstile", simTime);
 		ActivityStartCompare actStartComp = this.new ActivityStartCompare();
+		ActiveEntity turnstile = ents.get(0);
+		double turnstileID = getNumAttribute(turnstile, "ID", simTime, -1);
 		
 		if (idlePeople.size() > 0) {
 			Collections.sort(idlePeople, actStartComp);
+			// sort based on if they are at this turnstile
+			List<ActiveEntity> peopleThisTurnstile = idlePeople
+					  .stream()
+					  .filter(q -> getNumAttribute(q, "Turnstile", simTime, -1) == turnstileID)
+					  .collect(Collectors.toList());
 			
-			ActiveEntity turnstile = ents.get(0);
-			ActiveEntity person = idlePeople.get(0);
-					
-			transitionTo("Entry", person, turnstile);
+			// If someone waiting at this turnstile then transition
+			if (peopleThisTurnstile.size() > 0) {
+			
+				ActiveEntity person = peopleThisTurnstile.get(0);
+				transitionTo("Entry", person, turnstile);
+				
+			}
 		}
 	}
 	
-	public void StartChooseDirection(List<ActiveEntity> ents, double simTime) {
+	public void OnStartChooseDirection(List<ActiveEntity> ents, double simTime) {
 		
+		// Get attributes
+		ActiveEntity person = ents.get(0);
+		int currentsection = (int) getNumAttribute(person, "CurrentSection", simTime, -1);   
+		int seatsection = (int) getNumAttribute(person, "SeatSection", simTime, -1);  
+		// Test if in correct section
+		if (currentsection == seatsection) {
+			transitionTo("TakeSeat", person);
+		}
 		
+		// Otherwise traverse the section
+		int direction = (int) getNumAttribute(person, "Direction", simTime, -1);  
 		
-		
-		transitionTo()
-		
+		// Move down or up
+		transitionTo("TraverseSection", person);
+					
+					
+		if (direction == 1) {
+			// If necessary switch direction
+			if (seatsection < currentsection) {
+				setNumAttribute(person, "Direction", -1, DimensionlessUnit.class);
+			}
+		} else {
+			// If necessary switch direction
+			if (seatsection > currentsection) {
+				setNumAttribute(person, "Direction", 1, DimensionlessUnit.class);
+			}
+		}
+	
 	}
 	
+	
 }
+
+
+
+
+
