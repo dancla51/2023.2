7a7d4c53fa6b528fef47d236546a536670389aaa
dancla51 <dcla189@aucklanduni.ac.nz>
2023-10-19 15:32:28 +1300

    Working on 331

--- 331Lab5_FD/Lab_W1/functions_fd.py
+++ 331Lab5_FD/Lab_W1/functions_fd.py
@@ -44,14 +44,9 @@ class SolverPoissonXY(object):
         self.n = self.nx * self.ny
 
         # TODO: calculate the x and y values/coordinates of mesh as one-dimensional numpy arrays
-        # Goes across rows first
-        self.x = np.zeros([self.n])
-        self.y = np.zeros([self.n])
-        # For each row
-        for i in range(self.ny):
-            self.x[self.nx*i:self.nx*i+self.nx] = np.linspace(xlim[0], xlim[1], self.nx)
-        for j in range(self.nx):
-            self.y[self.ny*j:self.ny*j+self.ny] = np.linspace(ylim[0], ylim[1], self.ny)[j]
+        # X and Y vals
+        self.x = np.linspace(xlim[0], xlim[1], self.nx)
+        self.y = np.linspace(ylim[0], ylim[1], self.ny)
 
         # TODO: calculate the actual mesh spacing in x and y, may not be same as delta if not exactly divisible
         self.dx = (xlim[1]-xlim[0]) / self.nx
@@ -59,14 +54,14 @@ class SolverPoissonXY(object):
 
         # TODO: initialise the linear algebra matrices for, A solution = b
         self.a = np.zeros([self.n, self.n])
-        for r in range(self.n):
+        '''for r in range(self.n):
             if r == 0:
                 pass
             elif r == self.n-1:
-                pass
+                pass'''
 
         self.b = np.zeros([self.n])
-        self.solution = np.zeros([self.n])
+        self.solution = np.zeros([self.nx, self.ny])
 
         # store the four boundary conditions
         self.bc_x0 = bc_x0
@@ -81,7 +76,50 @@ class SolverPoissonXY(object):
         """
         Update the corresponding elements of the A matrix and b vector for Dirichlet boundary mesh points.
         """
-        pass
+        if self.bc_y0.get("type")=="dirichlet":
+            # Bottom edge
+            y = self.y[0]
+            # for each edge point
+            for i in range(self.nx):
+                x = self.x[i]
+                # b
+                self.b[i] = self.bc_y0.get("function")(x, y)
+                # a
+                self.a[i, i] = 1
+
+        if self.bc_y1.get("type")=="dirichlet":
+            # Top edge
+            y = self.y[-1]
+            # for each edge point
+            for i in range(self.nx):
+                x = self.x[i]
+                # b
+                print(self.nx*(self.ny-1)+i, self.bc_y1.get("function")(x, y))
+                self.b[self.nx*(self.ny-1)+i] = self.bc_y1.get("function")(x, y)
+                # a
+                self.a[self.nx*(self.ny-1)+i, self.nx*(self.ny-1)+i] = 1
+
+        if self.bc_x0.get("type")=="dirichlet":
+            # Left edge
+            x = self.x[0]
+            # for each edge point
+            for j in range(self.ny):
+                y = self.y[j]
+                # b
+                self.b[j*self.nx] = self.bc_x0.get("function")(x, y)
+                # a
+                self.a[j*self.nx, j*self.nx] = 1
+
+        if self.bc_x1.get("type") == "dirichlet":
+            # Right edge
+            x = self.x[-1]
+            # for each edge point
+            for j in range(self.ny):
+                y = self.y[j]
+                # b
+                self.b[j * self.nx + self.nx - 1] = self.bc_x1.get("function")(x, y)
+                # a
+                self.a[j * self.nx + self.nx - 1, j * self.nx + self.nx - 1] = 1
 
     def neumann(self):
         """
@@ -93,16 +131,42 @@ class SolverPoissonXY(object):
         """
         Update the corresponding elements of the A matrix and b vector for internal mesh points.
         """
-        pass
+        # Loop through each internal point
+        for i in range(1, self.nx-1):
+            for j in range(1, self.ny-1):
+                # K is number of 1D-sol / B / column of A
+                k = i + j * self.nx
+                self.a[k, k] = -2 / self.dx / self.dx - 2 / self.dy / self.dy
+                self.a[k, k - 1] = 1 / self.dx / self.dx
+                self.a[k, k + 1] = 1 / self.dx / self.dx
+                self.a[k, k - self.nx] = 1 / self.dy / self.dy
+                self.a[k, k + self.nx] = 1 / self.dy / self.dy
+                x = self.x[i]
+                y = self.y[j]
+                self.b[k] = self.poisson(x, y)
 
     def solve(self):
         """
         Call the dirichlet, neumann and internal methods to form the system of equations, Au=b. Then use built-in
         linear algebra solver to find u and reshape back to self.solution.
         """
-        pass
+        self.dirichlet()
+        self.neumann()
+        self.internal()
+        # Solve
+        u = np.linalg.solve(self.a, self.b)
+        self.solution = np.reshape(u, (self.ny, self.nx))
+
 
     def plot_solution(self):
         """
         Plot the PDE solution.
         """
+        X, Y = np.meshgrid(self.x, self.y)
+        Z = self.solution
+
+        # plot
+        levels = np.linspace(np.min(Z), np.max(Z), 20)
+        fig, ax = plt.subplots()
+        ax.contourf(X, Y, Z, levels=levels)
+        plt.show()
